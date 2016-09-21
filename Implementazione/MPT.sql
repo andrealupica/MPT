85840a6a8525d1a20d67560010c1400ce9cc2c8d
andrealupica <andrea.lupica@samtrevano.ch>
2016-09-21 13:20:19 +0200

    diario e DB

---
+++ Implementazione/MPT.sql
@@ -0,0 +1,54 @@
+drop database if exists MPT;
+create database MPT;
+use MPT;
+
+create table utente(
+	ute_nome varchar(30),
+	ute_cognome varchar(30),
+	ute_email varchar(50) primary key,
+	ute_password varchar(30),
+	ute_temppassword int default 1,
+	ute_docente int default 1,
+	ute_amministratore int default null,
+	ute_responsabile int default null
+);
+
+create table materia(
+	mat_id int primary key auto_increment,
+	mat_nome varchar(30)	
+);
+
+create table corso(
+	cor_id int primary key auto_increment,
+	cor_nome varchar(30),
+	cor_durata int
+);
+
+create table classe(
+	cla_id int primary key auto_increment,
+	cla_nome varchar(30),
+	cor_id int,
+	foreign key(cor_id) references corso(cor_id)
+);
+
+create table mat_app_cor(
+	mat_id int,
+	cor_id int,
+	primary key(mat_id,cor_id),
+	foreign key(mat_id) references materia(mat_id),
+	foreign key(cor_id) references corso(cor_id)
+);
+
+create table insegna(
+	ute_email varchar(30),
+	cla_id int,
+	mat_id int,
+	ins_ini_anno int,
+	ins_fin_anno int,
+	ins_ore_tot int,
+	ins_ore_AIT int,
+	primary key(ute_email,cla_id,mat_id,ins_ini_anno),
+	foreign key(ute_email) references utente(ute_email),
+	foreign key(cla_id) references classe(cla_id),
+	foreign key(mat_id) references materia(mat_id)
+);	
---
+++ Implementazione/fillMPT.sql
@@ -0,0 +1,20 @@
+insert into materia(mat_nome) values('italiano');
+insert into materia(mat_nome) values('tedesco');
+insert into materia(mat_nome) values('inglese');
+insert into materia(mat_nome) values('matematica');
+insert into materia(mat_nome) values('fisica');
+insert into materia(mat_nome) values('chimica');
+insert into materia(mat_nome) values('biologia');
+insert into materia(mat_nome) values('storia');
+insert into materia(mat_nome) values('economia');
+
+insert into corso(cor_id,cor_nome,cor_durata) values(1,'MP1 SPAI con AU',4);
+insert into corso(cor_id,cor_nome,cor_durata) values(2,'MP2 SPAI senza AU',4);
+insert into corso(cor_id,cor_nome,cor_durata) values(3,'MP1 SPAI (MPI)',3);
+insert into corso(cor_id,cor_nome,cor_durata) values(4,'MP1 SAMT Info',4);
+insert into corso(cor_id,cor_nome,cor_durata) values(5,'MP1 SAMT EM',4);
+insert into corso(cor_id,cor_nome,cor_durata) values(6,'MP1 SAMT Disegno',4);
+insert into corso(cor_id,cor_nome,cor_durata) values(7,'MP1 SAMT Chimica',3);
+insert into corso(cor_id,cor_nome,cor_durata) values(8,'MP2 Tecnica (CPQ)',1);
+insert into corso(cor_id,cor_nome,cor_durata) values(9,'MP2 Natura (CPQ)',1);
+
